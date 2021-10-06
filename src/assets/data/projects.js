2bac3962b51ed9a99b6eb06bc80b905d50263f2e
emilywilbur <ewilbur94@gmail.com>
2021-10-06 13:49:14 -0400

    projects section complete with styling//projects still need to be linked

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,46 +1,54 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
-import UTrackerImg from '../images/utracker.jpg';
-import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
-
+import Noods from '../images/noods.png';
+import Booze from '../images/booze.png';
+import Autumn from '../images/autumn.png';
+import Bullington from '../images/bullington.png';
+import Parks from '../images/parks.png';
+import Myrtle from '../images/myrtle.png'
 const projects = [
   {
     id: uuidv4(),
-    name: 'U Tracker',
+    name: 'Noods ATL',
     desc:
       'An application to track your all data from one place. I developed the website and the mobile app',
-    img: UTrackerImg,
+    img: Noods,
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
+    name: 'boozeMe',
     desc:
       'An app to help people to get an overview of how they can make the city beautiful.',
-    img: GreenCtgImg,
+    img: Booze,
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
-    desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    img: CoinTrackerImg,
-  },
-  {
-    id: uuidv4(),
-    name: "Cavin's Portfolio",
-    desc:
-      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    img: CavinImg,
-  },
-  {
-    id: uuidv4(),
-    name: 'Tracking Soft',
+    name: 'Myrtle Hill Consulting',
     desc:
       'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    img: ProjectImg,
+    img: Myrtle,
   },
+  {
+    id: uuidv4(),
+    name: 'Autumn Landscape',
+    desc:
+      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
+    img: Autumn,
+  },
+  {
+    id: uuidv4(),
+    name: "Bullington Clothing",
+    desc:
+      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
+    img: Bullington,
+  },
+  {
+    id: uuidv4(),
+    name: 'Parks&Parkas',
+    desc:
+      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
+    img: Parks,
+  },
+
 ];
 
 export default projects;
---
+++ src/components/ProjectItem.js
@@ -0,0 +1,59 @@
+import react from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import projectImg from "../assets/images/noods.png";
+
+const ProjectItemStyles = styled.div `
+.projectItem_img {
+    width: 100%;
+    height: 400px;
+    overflow: hidden;
+    border-radius: 12px;
+    display: inline-block;
+    border: 3px solid var(--gray-2);
+    img {
+        height; 100%;
+    }
+}
+.projectItem_info{
+    margin-top: 1rem;
+    background-color: var(--deep-dark);
+    padding: 1rem;
+    border-radius:12px;
+}
+.projectItem_title {
+    font-size: 2.2rem;
+    font-family: 'arial';
+}
+.projectItem_desc {
+    font-size: 1.6rem;
+    font-family: 'arial';
+    margin-top: 1rem;
+}
+@media only screen and (max-width: 768px) {
+    .projectItem_img{
+        height:150px;
+    }
+}
+
+`;
+
+export default function ProjectItem({
+    img= projectImg,
+    title= 'Project Name',
+    desc= 'lorem ipson description here',
+}) {
+    return(
+        <ProjectItemStyles>
+           <Link to="/projects" className="projectItem_img">
+               <img src={img} alt="project img" />
+           </Link>
+           <div className="projectItem_info">
+               <Link to="#">
+                   <h3 className="projectItem_title">{title}</h3>
+               </Link>
+               <p className="projectItem_desc">{desc}</p>
+           </div>
+        </ProjectItemStyles>
+    )
+}
